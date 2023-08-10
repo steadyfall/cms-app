dd2599ef86f8f4492b49e6f8fbbb731a832952f2
Himank Dave <himank.dave40@gmail.com>
2023-08-10 03:27:09 -0400

    made test, signin and signout view, added them to their respective routes

--- auth/urls.py
+++ auth/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import test, signin, signout
 
 urlpatterns = [
-    path("test/", name="test-page"),
+    path("test/", test, name="test-page"),
+    path("signin/", signin, name="signin"),
+    path("signout/", signout, name="signout"),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+from django.contrib import auth
+from .validate import usernameValidator, confirmPassword, passwordValidator
+
+
+def test(request):
+    return render(request, "auth/signin.html")
+
+
+def signin(request):
+    error: str = "Data is invalid. Try again."
+    if request.method == "POST":
+        try:
+            username: str = request.POST["username"]
+            password: str = request.POST["password"]
+        except KeyError:
+            messages.error(request, error)
+            return redirect("signin")
+
+        check: bool = usernameValidator(username) and passwordValidator(password)
+        if check:
+            if not User.objects.filter(username=username).exists():
+                messages.warning(
+                    request,
+                    "You don't have an account. Kindly check with admin before trying again.",
+                )
+                return redirect("signin")
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("signin")  # successful
+            else:
+                messages.error(request, "Wrong password. Try again.")
+                return redirect("signin")
+        else:
+            messages.error(request, error)
+            return redirect("signin")
+
+    return render(request, "auth/signin.html")
+
+
+def signout(request):
+    if not request.user.is_authenticated:
+        return redirect("signin") # logout
+    auth.logout(request)
+    return render(request, "auth/signout.html")
