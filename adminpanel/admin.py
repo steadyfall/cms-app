2f650a647caead405b9a7bfc0f01a38c7e9e8ee7
Himank Dave <himank.dave40@gmail.com>
2023-08-10 08:03:58 -0400

    reflected previous changes to admin.py

--- adminpanel/admin.py
+++ adminpanel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Plant, Zone, Case
 
-# Register your models here.
+admin.site.register(Plant)
+admin.site.register(Zone)
+admin.site.register(Case)
