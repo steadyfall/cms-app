ee3a4bfb76d8b3fffa9cb70d3ea058cb826d843d
Himank Dave <himank.dave40@gmail.com>
2023-08-10 03:41:50 -0400

    made route and views for admin signin and signout

--- auth/urls.py
+++ auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import test, signin, signout
+from .views import test, signin, signout, admin_signin, admin_signout
 
 urlpatterns = [
     path("test/", test, name="test-page"),
     path("signin/", signin, name="signin"),
     path("signout/", signout, name="signout"),
+    path("admin-signin/", admin_signin, name="admin-signin"),
+    path("admin-signout/", admin_signout, name="admin-signout"),
 ]
--- auth/views.py
+++ auth/views.py
@@ -48,3 +48,49 @@ def signout(request):
         return redirect("signin") # logout
     auth.logout(request)
     return render(request, "auth/signout.html")
+
+
+def admin_signin(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            redirect("test-page")
+        else:
+            return redirect("test-page")
+
+    error: str = "Data is invalid. Try again."
+    if request.method == "POST":
+        try:
+            username: str = request.POST["username"]
+            password: str = request.POST["password"]
+        except KeyError:
+            messages.error(request, error)
+            return redirect("admin-signin")
+
+        check: bool = usernameValidator(username)
+        if check:
+            if not User.objects.filter(username=username).exists():
+                messages.warning(
+                    request, "You don't have an account. Contact the head administrator."
+                )
+                return redirect("admin-signin")
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                if user.is_superuser:
+                    return redirect("test-page")
+                return redirect("test-page")
+            else:
+                messages.error(request, "Wrong password. Try again.")
+                return redirect("admin-signin")
+        else:
+            messages.error(request, error)
+            return redirect("admin-signin")
+
+    return render(request, "auth/admin_signin.html")
+
+
+def admin_signout(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return redirect("test-page")
+    auth.logout(request)
+    return render(request, "auth/admin_signout.html")
