60e64f9395dad7ce467ad87803d9063a5a67fe9f
Himank Dave <himank.dave40@gmail.com>
2023-08-11 05:48:31 -0400

    formatted code with Black

--- cmsUser/views.py
+++ cmsUser/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+
 class MainPage(View):
     def get(self, request, *args, **kwargs):
-        plant_name = ' (' + self.request.user.profile.assigned_plant.name + ')' if self.request.user.is_authenticated else ''
-        context = {
-            'title': f'Plant Management{plant_name}'
-        }
+        plant_name = (
+            " (" + self.request.user.profile.assigned_plant.name + ")"
+            if self.request.user.is_authenticated
+            else ""
+        )
+        context = {"title": f"Plant Management{plant_name}"}
         return render(request, "cmsUser/mainpage.html", context)
