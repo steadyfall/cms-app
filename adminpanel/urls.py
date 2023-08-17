26b8e7f02508344774e2614689e75edb5321dd7c
Himank Dave <himank.dave40@gmail.com>
2023-08-17 02:59:56 -0400

    created all required URLs

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,37 @@
 from django.urls import path
-from .views import test
+from .views import (
+    test,
+    AdminListDB,
+    ShowLogDB,
+    AdminDBObjectChange,
+    AdminDBObjectCreate,
+    AdminDBObjectDelete,
+    AdminDBObjectHistory,
+)
 
 urlpatterns = [
     path("test/", test, name="test-page"),
+    path("", test, name="adminMainPage"),
+    path("apps/<str:db>/", AdminListDB.as_view(), name="adminListDB"),
+    path("logs/", ShowLogDB.as_view(), name="adminListLogs"),
+    path(
+        "apps/<str:db>/object/<pk>/",
+        AdminDBObjectChange.as_view(),
+        name="adminDBObject",
+    ),
+    path(
+        "apps/<str:db>/create/",
+        AdminDBObjectCreate.as_view(),
+        name="adminDBObjectCreate",
+    ),
+    path(
+        "apps/<str:db>/object/<pk>/delete/",
+        AdminDBObjectDelete.as_view(),
+        name="adminDBObjectDelete",
+    ),
+    path(
+        "apps/<str:db>/object/<pk>/history/",
+        AdminDBObjectHistory.as_view(),
+        name="adminDBObjectHistory",
+    ),
 ]
