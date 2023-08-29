c206a5e88b84c7f57b4f5780f36f726c603128ad
Himank Dave <himank.dave40@gmail.com>
2023-08-29 18:26:42 -0400

    registered signals.py in apps.py

--- authorizer/apps.py
+++ authorizer/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthorizerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authorizer"
+
+    def ready(self):
+        import authorizer.signals
