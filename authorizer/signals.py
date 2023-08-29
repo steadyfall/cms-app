8a744c34f535f87d57e0be5ac83683239310f76b
Himank Dave <himank.dave40@gmail.com>
2023-08-29 18:38:21 -0400

    disabled signals for Profile-User relation

--- authorizer/signals.py
+++ authorizer/signals.py
@@ -4,7 +4,10 @@ from django.dispatch import receiver
 from .models import Profile
 
 
+# Fallback method, if UserChangeForm in ./adminpanel/forms.py does not work 
+"""
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+"""
